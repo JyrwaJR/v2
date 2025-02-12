dab5d9c3dc13cc6b6e14624859156e8fee9bd4da
JyrwaJr <jyrwaboys@gmail.com>
2025-02-12 22:53:06 +0530

    update

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,17 +1,17 @@
-import { type DocsLayoutProps } from 'fumadocs-ui/layout';
-import { type HomeLayoutProps } from 'fumadocs-ui/home-layout';
-import { pageTree } from '@/app/source';
+import { type DocsLayoutProps } from "fumadocs-ui/layout";
+import { type HomeLayoutProps } from "fumadocs-ui/home-layout";
+import { pageTree } from "@/app/source";
 
 // shared configuration
 export const baseOptions: HomeLayoutProps = {
   nav: {
-    title: 'Easy Docs',
+    title: "Jyrwa Docs",
   },
   links: [
     {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
+      text: "Documentation",
+      url: "/docs",
+      active: "nested-url",
     },
   ],
 };
